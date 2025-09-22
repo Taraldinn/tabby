1f7877943b41fff8f26880f59fbffe01fccef260
Taraldinn <kferdoush617@gmail.com>
2025-09-22 23:09:03 +0600

    Refactor logging formatter and improve code consistency in run-asgi.py

--- tabbycat/run-asgi.py
+++ tabbycat/run-asgi.py
@@ -12,7 +12,7 @@ root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 ch.setFormatter(formatter)
 root.addHandler(ch)
 
@@ -30,13 +30,13 @@ root.addHandler(ch)
 # Daphne Setup
 # ==============================================================================
 
-import asgi # noqa: E402
-from daphne.endpoints import build_endpoint_description_strings # noqa: E402
-from daphne.server import Server # noqa: E402
+import asgi  # noqa: E402
+from daphne.endpoints import build_endpoint_description_strings  # noqa: E402
+from daphne.server import Server  # noqa: E402
 
 # Docker/Heroku environments use NGINX and must bind to a socket; others bind to address
-if 'USING_NGINX' in os.environ and bool(int(os.environ['USING_NGINX'])):
-    root.info('TC_DEPLOY: Initialising Daphne with NGINX')
+if "USING_NGINX" in os.environ and bool(int(os.environ["USING_NGINX"])):
+    root.info("TC_DEPLOY: Initialising Daphne with NGINX")
     Server(
         application=asgi.application,
         endpoints=build_endpoint_description_strings(
@@ -44,7 +44,7 @@ if 'USING_NGINX' in os.environ and bool(int(os.environ['USING_NGINX'])):
         ),
         ping_interval=15,
         ping_timeout=30,
-        websocket_timeout=10800, # 3 hours maximum length
+        websocket_timeout=10800,  # 3 hours maximum length
         websocket_connect_timeout=10,
         application_close_timeout=10,
         verbosity=2,
@@ -53,16 +53,18 @@ if 'USING_NGINX' in os.environ and bool(int(os.environ['USING_NGINX'])):
         proxy_forwarded_proto_header="X-Forwarded-Proto",
     ).run()
 else:
-    root.info('TC_DEPLOY: Initialising Daphne with Host/Port')
+    # Render provides the PORT environment variable
+    port = os.environ.get("PORT", "8000")
+    root.info(f"TC_DEPLOY: Initialising Daphne with Host/Port - 0.0.0.0:{port}")
     Server(
         application=asgi.application,
         endpoints=build_endpoint_description_strings(
             host="0.0.0.0",
-            port="8000",
+            port=port,
         ),
         ping_interval=15,
         ping_timeout=30,
-        websocket_timeout=10800, # 3 hours maximum length
+        websocket_timeout=10800,  # 3 hours maximum length
         websocket_connect_timeout=10,
         application_close_timeout=10,
         verbosity=2,
